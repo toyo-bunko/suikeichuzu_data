a63f5404bdf7a45d962adfdf30a4d5024146e54c
nakamura196 <na.kamura.1263@gmail.com>
2021-07-02 09:29:05 +0900

    fix: bug

--- src/500_book.py
+++ src/500_book.py
@@ -13,28 +13,58 @@ import pandas as pd
 import urllib.parse
 import copy
 
-excel_path = "/Users/nakamurasatoru/git/d_toyo/app-suikeichuuzu/src/data/水経注図冊子画像-区画対応.xlsx"
+def handleManifests():
 
-df = pd.read_excel(excel_path, sheet_name=0, header=None, index_col=None, engine='openpyxl')
+    files = glob.glob("../docs/iiif/sjzt_*/manifest.json")
 
-r_count = len(df.index)
-c_count = len(df.columns)
+    images_map = {}
 
-docs = {}
+    for file in files:
 
-for j in range(1, r_count):
-    c0 = df.iloc[j, 0]
-    c1 = df.iloc[j, 1]
-    c2 = df.iloc[j, 2].replace("冊", "")
-    # print(id)
-    docs[c0+"-"+c1+"-"+c2] = df.iloc[j, 3]
+        with open(file) as f:
+            m_data = json.load(f)
 
-print(docs)
+        canvases = m_data["sequences"][0]["canvases"]
+
+        for canvas in canvases:
+            id = canvas["@id"]
+            filename = canvas["images"][0]["resource"]["@id"].split("/")[-1]
+            images_map[filename] = {
+                "canvas" : id,
+                "manifest" : file.replace("../docs", "https://static.toyobunko-lab.jp/suikeichuzu_data")
+            }
+
+    # print(images_map.keys())
+
+    return images_map
+
+def handleMap():
+
+    excel_path = "/Users/nakamurasatoru/git/d_toyo/app-suikeichuuzu/src/data/水経注図冊子画像-区画対応.xlsx"
+
+    df = pd.read_excel(excel_path, sheet_name=0, header=None, index_col=None, engine='openpyxl')
+
+    r_count = len(df.index)
+    c_count = len(df.columns)
+
+    docs = {}
+
+    for j in range(1, r_count):
+        c0 = df.iloc[j, 0]
+        c1 = df.iloc[j, 1]
+        c2 = df.iloc[j, 2].replace("冊", "")
+        # print(id)
+        docs[c0+"-"+c1+"-"+c2] = df.iloc[j, 3]
+
+    return docs
+
+# print(docs)
 
 with open("/Users/nakamurasatoru/git/d_toyo/suikeichuzu/static/data/index_river.json") as f:
     dd = json.load(f)
 
-map = {}
+docs = handleMap()
+images = handleManifests()
 
 for item in dd:
     表裏 = item["表裏"][0]
@@ -49,31 +79,26 @@ for item in dd:
     else:
         slug = "{}{}-{}-{}".format(区画南北, 区画東西, 表裏, 冊)
 
-    '''
-    if slug not in map:
-        map[slug] = []
-    map[slug].append(item["objectID"])
-    '''
-    print(item["objectID"])
+    id = item["objectID"]
+
+    # print(item["objectID"])
     if slug in docs:
-        print(docs[slug])
+        # print()
+        filename = docs[slug]
+        info = images[filename]
 
-# for docs
+        item["水経注図：冊子画像"] = ["http://www.toyo-bunko.or.jp/research/ss/iiif/mirador/?manifest=" + info["manifest"] + "&canvas=" + info["canvas"]]
 
-'''
 
-with open("/Users/nakamurasatoru/git/d_toyo/app-suikeichuuzu/static/data/curation.json") as f:
-    dd = json.load(f)
 
-selections = dd["selections"]
+    with open("/Users/nakamurasatoru/git/d_toyo/suikeichuzu/static/data/item/"+id+".json", 'w') as outfile:
+        json.dump(item, outfile, ensure_ascii=False,
+                    indent=4, sort_keys=True, separators=(',', ': '))
 
-for selection in selections:
-    print(selection["within"])
+    if "水経注図：冊子画像" in item:
+        del item["水経注図：冊子画像"]
 
-'''
 
-'''
-with open("/Users/nakamurasatoru/git/d_toyo/suikeichuzu/static/data/index.json", 'w') as outfile:
-    json.dump(actions, outfile, ensure_ascii=False,
+with open("/Users/nakamurasatoru/git/d_toyo/suikeichuzu/static/data/index_book.json", 'w') as outfile:
+    json.dump(dd, outfile, ensure_ascii=False,
                 indent=4, sort_keys=True, separators=(',', ': '))
-'''
